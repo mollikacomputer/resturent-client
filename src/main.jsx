33ddfd33e53e577c48772740ecbcf3fcdbe277dd
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-10-19 08:45:51 +0600

    common section added navbar footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import { router } from './routes/Routs';
+import {
+  RouterProvider,
+} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='max-w-screen-lg mx-auto'>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
